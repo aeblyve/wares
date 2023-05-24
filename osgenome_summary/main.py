4cd39b65fdb7f85c4d27d4933fde58e623f66204
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-05-24 18:23:07 -0400

    init

---
+++ osgenome_summary/main.py
@@ -0,0 +1,48 @@
+import openai
+import sys
+import openpyxl
+
+openai.api_key = "<CHANGEME>"
+
+def slice_between_substrings(s, start_sub, end_sub):
+    start = s.find(start_sub)
+    end = s.find(end_sub)
+    if start != -1 and end != -1 and start < end: # ensure both substrings are in s and start_sub is before end_sub
+        return s[start + len(start_sub) : end]
+    else:
+        return None # or some other value indicating that the slice couldn't be made
+
+input_file = sys.argv[1]
+
+workbook = openpyxl.load_workbook(input_file)
+worksheet = workbook.active
+
+phenotypes = []
+
+for row in worksheet.iter_rows(min_col=5, max_col=5):
+    for cell in row:
+        sliced_value = slice_between_substrings(cell.value, "<b>", "</b>")
+        if sliced_value is not None:
+            #print(sliced_value)
+            phenotypes.append(sliced_value)
+
+phenotype_report = "\n".join(phenotypes)
+
+response = openai.ChatCompletion.create(
+  model="gpt-4",
+  messages=[
+        {"role": "system", "content": "I am going to send you a report of phenotypes as derived from a SNP analysis. You job is to summarize the report into one paragraph. Focus on the genetic traits, with little exposition."},
+        {"role": "user", "content": phenotype_report},
+    ]
+)
+
+print(response['choices'][0]['message']['content'])
+
+
+
+
+
+
+
+
+

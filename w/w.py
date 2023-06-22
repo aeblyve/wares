e4e180b25aeace7312e9ce83418c244db71abb25
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-21 21:25:58 -0400

    added user again

--- w/w.py
+++ w/w.py
@@ -1,13 +1,14 @@
 import sys, requests, time, json, folium
 
 IP_API_URL = "http://ip-api.com/json/"
-
+user = set()
 
 m = folium.Map()
 with open(sys.argv[1]) as w:
     for line in w:
         tokens = line.split()
         if tokens[0] not in user: # handle multiple terminals by the same user.
+            user.add(tokens[0])
             r = json.loads(requests.get(IP_API_URL + tokens[2]).content)
             time.sleep(5) # avoid throttling
             if "lat" in r and "lon" in r:

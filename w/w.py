5e52bb5b8b9f6cb9dc2d0dde51ab8b2c65d7e571
Leonid Belyaev <belyaev.l@northeastern.edu>
2023-06-21 21:27:38 -0400

    Make readable

--- w/w.py
+++ w/w.py
@@ -1,17 +1,17 @@
 import sys, requests, time, json, folium
 
 IP_API_URL = "http://ip-api.com/json/"
-user = set()
+users = set()
 
 m = folium.Map()
 with open(sys.argv[1]) as w:
     for line in w:
-        tokens = line.split()
-        if tokens[0] not in user: # handle multiple terminals by the same user.
-            user.add(tokens[0])
-            r = json.loads(requests.get(IP_API_URL + tokens[2]).content)
+        user, pty, ip, cmd = line.split()
+        if user not in users: # handle multiple terminals by the same user.
+            users.add(user)
+            r = json.loads(requests.get(IP_API_URL + ip).content)
             time.sleep(5) # avoid throttling
             if "lat" in r and "lon" in r:
-                folium.Marker([r["lat"], r["lon"]], tooltip=tokens[2], popup=tokens[0]).add_to(m)
+                folium.Marker([r["lat"], r["lon"]], tooltip=ip, popup=user).add_to(m)
 
 m.save("index.html")
